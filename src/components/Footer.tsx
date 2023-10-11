fe282c5228406e1c76a01e218bb5a374813f8b12
Kyaw Soe Thant <kyawsoethant@abank.com.mm>
2023-10-11 14:51:46 +0630

    [ENHANCEMENT]Modify Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="h-12 md:h-24 flex p-4 lg:px-20 xl:px-10 items-center justify-between text-red-500 uppercase">
+    <div className="h-16 max-sm:py-10 max-sm:px-5 md:h-24 md:px-6 flex lg:px-20 xl:px-10 items-center justify-between text-red-500 uppercase">
       <Link href="/" className="font-bold text-xl">
         Olive Garden
       </Link>
